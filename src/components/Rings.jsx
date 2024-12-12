4d515932b6dd96f0cc6cfc4bb5f572930b03cf1f
QuantumMarttY-tape2 <quantummartty@gmail.com>
2024-12-11 22:12:16 -0500

    Update Rings.jsx

--- src/components/Rings.jsx
+++ src/components/Rings.jsx
@@ -11,7 +11,7 @@ const Rings = ({ position }) => {
     }
   }, []);
 
-  const texture = useTexture('textures/rings.png');
+  const texture = useTexture('/public/textures/rings.png');
 
   useGSAP(
     () => {
