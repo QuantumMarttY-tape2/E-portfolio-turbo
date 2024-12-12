a115e78d9ea13ef50df35619c32f63e7977c4b26
QuantumMarttY-tape2 <quantummartty@gmail.com>
2024-12-11 22:11:37 -0500

    Update Hydrogen.jsx

--- src/components/Hydrogen.jsx
+++ src/components/Hydrogen.jsx
@@ -10,7 +10,7 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 const Hydrogen = (props) => {
-  const { nodes, materials } = useGLTF('/models/hydrogen.glb')
+  const { nodes, materials } = useGLTF('/public/models/hydrogen.glb')
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
